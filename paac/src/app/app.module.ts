75d942b9290c623d24d9c3fbe5b12e303915b325
carsonchen1129@gmail.com <carsonchen1129@gmail.com>
2018-05-07 13:43:26 -0400

    Initial commit of the application. Current stage: 1. Show Google Map. 2. Add a marker on the map. 3. switch languages 4. connect to firebase
    Initial commit of the application. Current stage: 1. Show Google Map.
    2. Add a marker on the map. 3. switch languages 4. connect to firebase

---
+++ paac/src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDP9a_TdLEPhuCmgnDgjrUTJRQQxjdOBl4",
+  authDomain: "paac-1525669965295.firebaseapp.com",
+  databaseURL: "https://paac-1525669965295.firebaseio.com",
+  projectId: "paac-1525669965295",
+  storageBucket: "paac-1525669965295.appspot.com",
+  messagingSenderId: "73041968709"
+};
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    HttpClient,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
